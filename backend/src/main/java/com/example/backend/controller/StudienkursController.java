93ad0fa53ba121aaed4dbbd0cf8eab13a1190dbf
Dustin Vetter <dustinvetter@icloud.com>
2026-05-11 21:37:29 +0200

    remove @CrossOrigin in backend controllers - nginx handles it

--- backend/src/main/java/com/example/backend/controller/StudienkursController.java
+++ backend/src/main/java/com/example/backend/controller/StudienkursController.java
@@ -2,7 +2,6 @@ package com.example.backend.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.backend.entity.Studienkurs;
 import com.example.backend.repository.StudienkursRepository;
 
-@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:30080"})
 @RestController
 @RequestMapping("/api/studienkurse")
 public class StudienkursController {
